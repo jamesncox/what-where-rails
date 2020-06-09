23541a8dcf27a6b24c7493f011a4f7d1b80f3df7
jamesncox <jamesncox@yahoo.com>
2020-06-09 08:51:46 -0500

    Add relationships and validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :stores
+    has_many :items, through: :stores
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password_confirmation, presence: true, on: :create
 end
