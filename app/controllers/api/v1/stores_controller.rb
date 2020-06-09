15a0e2927a0baf05dd7082aa1438952a3919f280
jamesncox <jamesncox@yahoo.com>
2020-06-09 09:54:06 -0500

    Add stores controller

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,2 +1,36 @@
 class Api::V1::StoresController < ApplicationController
+    def index
+        @stores = Store.all 
+        render json: @stores, include: :items, status: 200
+    end
+
+    def show 
+        @store = Store.find_by(id: params[:id])
+        render json: @store, include: :items, status: 200
+    end 
+
+    def create 
+        @store = Store.new(store_params)
+        if @store.save
+            render json: @store, include: :items, status: 200
+        else
+            render json: { errors: @store.errors.full_messages}, status: 400
+        end
+    end 
+
+    def update 
+        @store = Store.find_by(id: params[:id])
+        @store.update(store_params)
+        render json: @store, include: :items, status: 200
+    end
+
+    def destroy
+        @store.destroy
+        head :no_content
+    end 
+
+    private 
+        def store_params
+            params.require(:store).permit(:name, :type, :color, :user_id)
+        end
 end
