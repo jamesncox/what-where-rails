a5c850a421d699755f976ed145859948d312ba68
jamesncox <jamesncox@yahoo.com>
2020-06-08 21:59:01 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :stores
-  resources :items
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+
+      resources :users, only: [:create, :destroy]
+      resources :stores
+      resources :items
+
+       # sessions CSRF-TOKEN route
+       get '/auth_check' => 'sessions#auth_check'
+
+       # /signup is my post fetch request route for signing up new user to users#create action
+       post '/signup' => 'users#create'
+ 
+       # /login is my post fetch requeset route for logging in existing user to sessions#create action
+       post '/login' => 'sessions#create'
+ 
+       # /current_user endpoint for a fetch on refresh in App.js, to persist User on refresh
+       get '/current_user' => 'users#current_user'
+ 
+       # /logout is my post fetch request route for logging out of existing user's session, sessions#destroy
+       delete "/logout" => "sessions#destroy"
+      
+    end
+  end
 end
