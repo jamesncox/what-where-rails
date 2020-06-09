47ab6a080b1cc9a8f1eedc71b622b17c68b1908e
jamesncox <jamesncox@yahoo.com>
2020-06-09 09:15:55 -0500

    Update Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
     belongs_to :store
     has_one :user, through: :stores
 
-    validats :name, presence: true
+    validates :name, presence: true
     validates :price, presence: true
     validates :quantity, presence: true
 end
