13615dc8bfe36d4bce7ad693c163755351cbf742
jamesncox <jamesncox@yahoo.com>
2020-06-09 08:47:10 -0500

    Add validations and relationships to Store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,8 @@
 class Store < ApplicationRecord
+    belongs_to :user
+    has_many :items
+
+    validates :name, presence: true
+    validates :type, presence: true
+    validates :color, presence: true 
 end
