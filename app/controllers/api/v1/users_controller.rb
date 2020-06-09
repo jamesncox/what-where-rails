74b4ce852fad8d2e222f1b910818d47e52e37d65
jamesncox <jamesncox@yahoo.com>
2020-06-08 21:58:00 -0500

    Move controllers to api/v1 and create User controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,28 @@
+class API::V1::UsersController < ApplicationController
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            log_in(@user)
+            render json: @user, status: 200
+        else 
+            render json: { errors: @user.errors.full_messages }, status: 400
+        end
+    end
+
+    def current_user
+        @user = User.find_by(id: session[:user_id])
+        if @user
+            render json: @user, except: [:password_digest, :created_at, :updated_at], status: 200
+        end
+    end
+
+    def destroy
+        @user.destroy
+        head :no_content
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :password, :password_confirmation)
+        end
+end
