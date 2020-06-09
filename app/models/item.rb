5d890ecec3e184c9ba6892f1b678c947f1077caa
jamesncox <jamesncox@yahoo.com>
2020-06-09 08:45:15 -0500

    Add relationships and validations to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+    belongs_to :store
+    has_one :user, through: :stores
+
+    validats :name, presence: true
+    validates :price, presence: true
+    validates :quantity, presence: true
 end
