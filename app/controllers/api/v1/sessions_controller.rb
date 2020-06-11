2f14611f78b830978f764cc303445511c4db8f08
jamesncox <jamesncox@yahoo.com>
2020-06-11 11:29:41 -0500

    Debug sessions_controller/users controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,12 +6,12 @@ class Api::V1::SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
-            if @user && @user.authenticate(params[:user][:password])
-                session[:user_id] = @user.id 
-                render json: @user, status: 200
-            else 
-                render json: { errors: ["Unable to log in. Username and Password are case-sensitive. Check credentials and please try again."] }, status: 400
-            end
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            render json: @user, status: 200
+        else 
+            render json: { errors: ["Unable to log in. Username and Password are case-sensitive. Check credentials and please try again."] }, status: 400
+        end
     end
 
     def destroy
